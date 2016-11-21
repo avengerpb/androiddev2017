72678c5a927e169c3e67adade764571ae19714c5
haanh764 <ngohaanh764@gmail.com>
2016-11-22 00:02:24 +0700

    Toggle and back on device

---
+++ Github_client/app/src/main/java/com/example/sieunhan/github_client/fragment/IssueFragment.java
@@ -0,0 +1,25 @@
+package com.example.sieunhan.github_client.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sieunhan.github_client.R;
+
+
+public class IssueFragment extends Fragment {
+    public IssueFragment() {
+        super();
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_issue, container, false);
+        return v;
+    }
+}
