e9ae82f59185919ff02e947b9b1409afa6d4f412
Nguyen Minh Duc <nguyenminhduc1803@gmail.com>
2016-12-02 20:44:35 +0700

    Create Gists Activity (including Toolbar and TabLayout)

---
+++ Github_client/app/src/main/java/com/example/sieunhan/github_client/GistsActivity.java
@@ -0,0 +1,70 @@
+package com.example.sieunhan.github_client;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.example.sieunhan.github_client.fragment.RepoFragment;
+
+/**
+ * Created by Sieu Nhan on 12/2/2016.
+ */
+
+public class GistsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gists);
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.gists_pager);
+        pager.setOffscreenPageLimit(5);
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.gists_tab);
+        tabLayout.setupWithViewPager(pager);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.gists_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 5;
+        private String titles[] = new String[] { "MINE", "STARRED", "ALL" };
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+        @Override
+        public Fragment getItem(int page) {
+            switch (page) {
+                case 0: return new RepoFragment();
+                case 1: return new RepoFragment();
+                case 2: return new RepoFragment();
+            }
+            return new RepoFragment();
+        }
+        @Override
+        public CharSequence getPageTitle(int page) {
+            return titles[page];
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.gists,menu);
+        return true;
+    }
+}
